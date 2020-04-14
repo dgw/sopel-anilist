4945d4b37824e89fa6858ef22be2a7b7948e8bdb
SleepingPanda <>
2020-04-14 10:23:47 -0400

    Anime Lookup
    Basic anime lookup command with no error handling. Will need @dgw to weigh in.
    
    Co-Authored-By: dgw <dgw@users.noreply.github.com>

--- anilist.py
+++ anilist.py
@@ -17,6 +17,43 @@ from sopel.tools import web
 
 ANILIST_ENDPOINT = "https://graphql.anilist.co/"
 QUERIES = {
+    "anime": """
+        query ($name: String) {
+            Media (search: $name, type: ANIME) {
+                title {
+                    romaji
+                    english
+                }
+                format
+                seasonYear
+                averageScore
+                status
+                episodes
+                siteUrl
+                genres
+                description (asHtml: true)
+                characters (role: MAIN) {
+                    edges {
+                        node {
+                            name {
+                                full
+                            }
+                        }
+                        voiceActors (language: JAPANESE) {
+                            name {
+                                full
+                            }
+                        }
+                    }
+                }
+                studios (isMain: true) {
+                    nodes {
+                        name
+                    }
+                }
+            }
+        }
+    """,
     "character": """
         query ($name: String) {
             Character(search: $name) {
@@ -44,6 +81,24 @@ QUERIES = {
         }
     """,
 }
+
+
+@module.commands('anilist', 'al')
+def al_anime(bot, trigger):
+    """Queries AniList for an anime matching the search input."""
+    if not trigger.group(2):
+        return
+
+    variables = {
+        'name': trigger.group(2),
+    }
+    result = requests.post(ANILIST_ENDPOINT, json={'query': QUERIES['anime'], 'variables': variables})
+    data = result.json()['data']
+
+    if data:
+        bot.say(data['Media']['title']['english'] + ' (' + data['Media']['seasonYear'] + ') | ' + data['Media']['format'] + ' | Studio: ' + data['Media']['studios']['nodes']['name'] + ' | Score: ' + data['Media']['averageScore'] + ' | ' + data['Media']['status'] + ' Eps: ' + data['Media']['episodes'] + ' | ' + data['Media']['siteUrl'] + ' | Genres: ' + data['Media']['genres'] + ' | VA: ' + data['Media']['characters']['edges']['voiceActors'] + ' | Synopsis: ' + data['Media']['description'])
+    else:
+        bot.reply("No results found for '%s'." % query)
 
 
 @module.commands('anilistchar', 'alc')
